2cf54c103d2d631d4d9c99fb9079730995449d2e
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-08-16 17:53:26 +0300

    1.4

--- BlazorApp11/Data/Grocery.cs
+++ BlazorApp11/Data/Grocery.cs
@@ -1,8 +1,13 @@
-﻿namespace BlazorApp11.Data
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorApp11.Data
 {
     public class Grocery
     {
-        public string? Name { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters.")] 
+        public string Name { get; set; }
+        [Required]
+        [Range(1,10000, ErrorMessage = "Valid Price Range is (1-10000)")]
         public float Price { get; set; }
     }
 }
